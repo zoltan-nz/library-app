5f5e126d68a3e3cdd4d24752d5c98de430deec13
Zoltan <zdebre@gmail.com>
2015-09-14 22:44:55 +1200

    Add contact page

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('about');
+  this.route('contact');
 });
 
 export default Router;
---
+++ tests/unit/routes/contact-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:contact', 'Unit | Route | contact', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
