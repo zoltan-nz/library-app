11b50cce0352c0185ddc3634222ca18f75421b1a
Zoltan <zdebre@gmail.com>
2015-10-26 16:42:39 +1300

    nav-link-to update to keep compatibility with browsers

--- app/components/nav-link-to.js
+++ app/components/nav-link-to.js
@@ -1,5 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.LinkComponent.extend({
-  tagName: 'li'
+    tagName: 'li',
+
+    hrefForA: Ember.computed('models', 'qualifiedRouteName', function computeLinkToComponentHref() {
+        let qualifiedRouteName = this.get('qualifiedRouteName');
+        let models = this.get('models');
+
+        if (this.get('loading')) {
+            return this.get('loadingHref');
+        }
+
+        let routing = this.get('_routing');
+        let queryParams = this.get('queryParams.values');
+        return routing.generateURL(qualifiedRouteName, models, queryParams);
+    })
 });
