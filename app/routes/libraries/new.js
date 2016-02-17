1636d4416ad111964f8fca4ad1c916fcb45b5d46
Zoltan <zdebre@gmail.com>
2016-02-17 23:52:26 +1300

    Update ember-cli

--- app/routes/libraries/new.js
+++ app/routes/libraries/new.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  model: function () {
+  model() {
     return this.store.createRecord('library');
   },
 
-  setupController: function (controller, model) {
+  setupController(controller, model) {
     this._super(controller, model);
 
     controller.set('title', 'Create a new library');
