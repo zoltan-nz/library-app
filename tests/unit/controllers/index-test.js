75ce93270e48e447b588e63978befba0249d4f28
Zoltan <zdebre@gmail.com>
2016-11-29 18:22:01 +1300

    Update to Ember 2.10

--- tests/unit/controllers/index-test.js
+++ tests/unit/controllers/index-test.js
@@ -1,12 +1,12 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('controller:index', {
+moduleFor('controller:index', 'Unit | Controller | index', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
 
 // Replace this with your real tests.
 test('it exists', function(assert) {
-  var controller = this.subject();
+  let controller = this.subject();
   assert.ok(controller);
 });
