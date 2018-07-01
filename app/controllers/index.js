41b9687ef5d259ac85771d975635fadcd54e3e05
Zoltan <zdebre@gmail.com>
2018-07-01 22:07:00 +1200

    Update text on home page

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,7 +3,7 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
 
-  headerMessage: 'Coming Soon',
+  headerMessage: 'Demo Home Page',
   responseMessage: '',
   emailAddress: '',
 
