56ba461a9d5ddb728fcdf6b94a8badd9f2aada92
Zoltan <zdebre@gmail.com>
2018-09-28 11:46:00 +1200

    Add sourcemap to babel config

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,10 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    babel: {
+      sourceMaps: 'inline'
+    }
+
     // Add options here
   });
 
