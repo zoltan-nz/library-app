628f5a69e5e9c68ca3905a6f9788e2390e21cc85
Tom Kraak <thomas.kraak@teamaol.com>
2019-05-14 23:02:52 -0400

    add unit tests for contact route

--- tests/unit/routes/contact-test.js
+++ tests/unit/routes/contact-test.js
@@ -1,11 +1,53 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import EmberObject from '@ember/object';
+import sinon from 'sinon';
 
-module('Unit | Route | contact', function(hooks) {
+const { spy, stub } = sinon;
+
+module('Unit | Route | contact', hooks => {
   setupTest(hooks);
 
-  test('it exists', function(assert) {
-    let route = this.owner.lookup('route:contact');
-    assert.ok(route);
+  hooks.beforeEach(function () {
+    this.createRecord = stub().returns('contact');
+    this.destroyRecord = spy();
+    this.route = this.owner.factoryFor('route:contact').create({
+      controller: EmberObject.create({
+        model: EmberObject.create({
+          isNew: true,
+          destroyRecord: this.destroyRecord
+        })
+      }),
+      store: { createRecord: this.createRecord }
+    });
+  });
+
+  test('model hook', function(assert) {
+    assert.expect(2);
+    assert.equal(this.route.model(), 'contact');
+    assert.ok(this.createRecord.calledOnceWith('contact'));
+  });
+
+  test('sendMessage action', function(assert) {
+    const save = stub().returns({ then: stub().yields() });
+    const newContactMessage = { save };
+    this.route.send('sendMessage', newContactMessage);
+    assert.expect(2);
+    assert.ok(save.calledOnce);
+    assert.ok(this.route.controller.get('responseMessage'));
+  });
+
+  test('willTransition action', function(assert) {
+    const { destroyRecord, route } = this;
+    route.send('willTransition');
+    assert.expect(4);
+    assert.ok(destroyRecord.calledOnce);
+    assert.notOk(route.controller.get('responseMessage'));
+    destroyRecord.resetHistory();
+
+    route.controller.model.set('isNew', false);
+    route.send('willTransition');
+    assert.ok(destroyRecord.notCalled);
+    assert.notOk(route.controller.get('responseMessage'));
   });
 });
