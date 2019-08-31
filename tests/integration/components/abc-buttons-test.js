ce84a808f4152eb1b485b9df83103a9eb4e59a9e
Tom Kraak <tom.kraak@gmail.com>
2019-08-31 15:56:32 -0400

    add integration test for abc-buttons component

---
+++ tests/integration/components/abc-buttons-test.js
@@ -0,0 +1,22 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import letters from '../../fixtures/alphabet';
+
+module('Integration | Component | abc-buttons', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+
+    await render(hbs`{{abc-buttons}}`);
+
+    const component = this.element.querySelector('div');
+    const children = [...component.children];
+
+    assert.expect(28);
+    assert.ok(component.classList.contains('btn-group'));
+    assert.equal(component.childElementCount, 26);
+    children.forEach((child, i) => assert.equal(child.textContent, letters[i]));
+  });
+});
