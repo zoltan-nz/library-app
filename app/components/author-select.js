d185ad59843bc835fdd4197d3556230f52e13679
Zoltan <zdebre@gmail.com>
2020-02-19 00:01:00 +1300

    Update AuthorSelect Component

--- app/components/author-select.js
+++ app/components/author-select.js
@@ -1,18 +1,18 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import {tracked} from "@glimmer/tracking";
+import {action} from "@ember/object";
 
-export default Component.extend({
-
-  tagName: 'select',
-  classNames: ['form-control'],
-  authors: null,
-  book: null,
-
+export default class AuthorSelectComponent extends Component {
+  @tracked authors = this.args.authors;
+  @tracked book = this.args.book;
+  
   //pass an action to override
-  onChange() {},
+  @tracked onChange = this.args.onChange;
 
+  @action
   change(event) {
     const selectedAuthorId = event.target.value;
     const selectedAuthor = this.authors.find((record) => record.id === selectedAuthorId);
     this.onChange(selectedAuthor, this.book);
   }
-});
+}
