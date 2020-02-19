1dab915201efced83a9bebb23deaa3d057109deb
Zoltan <zdebre@gmail.com>
2020-02-19 23:29:00 +1300

    Update AbcButtons Component

--- app/components/abc-buttons.js
+++ app/components/abc-buttons.js
@@ -1,12 +1,7 @@
-import { computed } from '@ember/object';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 
-export default Component.extend({
-  classNames: ['btn-group'],
-
-  atoz: computed(function() {
-
-    // Source: http://stackoverflow.com/questions/12376870/create-an-array-of-characters-from-specified-range
+export default class AbcButtonsComponent extends Component {
+  get atoz() {
     return Array.apply(null, { length: 26 }).map((x, i) => String.fromCharCode(97 + i).toUpperCase());
-  })
-});
+  }
+}
