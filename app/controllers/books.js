5c775babb1b7db3b1046b297137dcee9be01931a
Zoltan <zdebre@gmail.com>
2020-03-08 16:15:37 +1300

    Update Books Controller, Route and template #49

--- app/controllers/books.js
+++ app/controllers/books.js
@@ -1,77 +1,85 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 import { alias } from '@ember/object/computed';
 
-export default Controller.extend({
-  authors: alias('model.authors'),
-  books: alias('model.books'),
-  libraries: alias('model.libraries'),
+export default class BooksController extends Controller {
+  @alias('model.authors') authors;
+  @alias('model.books') books;
+  @alias('model.libraries') libraries;
 
-  actions: {
-    editBook(book) {
-      book.set('isEditing', true);
-    },
-
-    cancelBookEdit(book) {
-      book.set('isEditing', false);
-      book.rollbackAttributes();
-    },
-
-    saveBook(book) {
-      if (book.get('isNotValid')) {
-        return;
-      }
-
-      book.set('isEditing', false);
-      book.save();
-    },
-
-    editAuthor(book) {
-      book.set('isAuthorEditing', true);
-    },
-
-    cancelAuthorEdit(book) {
-      book.set('isAuthorEditing', false);
-      book.rollbackAttributes();
-    },
-
-    saveAuthor(author, book) {
-      // Firebase adapter is buggy, we have to manually remove the previous relation
-      book.get('author').then((previousAuthor) => {
-        previousAuthor.get('books').then((previousAuthorBooks) => {
-          previousAuthorBooks.removeObject(book);
-          previousAuthor.save();
-        });
-      });
-
-      // Setup the new relation
-      book.set('author', author);
-      book.save().then(() => author.save());
-      book.set('isAuthorEditing', false);
-    },
-
-    editLibrary(book) {
-      book.set('isLibraryEditing', true);
-    },
-
-    cancelLibraryEdit(book) {
-      book.set('isLibraryEditing', false);
-      book.rollbackAttributes();
-    },
-
-    saveLibrary(library, book) {
-      // Firebase adapter is buggy, we have to manually remove the previous relation.
-      // You don't need this callback mess when your adapter properly manages relations.
-      // If Firebase fix this bug, we can remove this part.
-      book.get('library').then((previousLibrary) => {
-        previousLibrary.get('books').then((previousLibraryBooks) => {
-          previousLibraryBooks.removeObject(book);
-          previousLibrary.save();
-        });
-      });
-
-      book.set('library', library);
-      book.save().then(() => library.save());
-      book.set('isLibraryEditing', false);
-    }
+  @action
+  editBook(book) {
+    book.set('isEditing', true);
   }
-});
+
+  @action
+  cancelBookEdit(book) {
+    book.set('isEditing', false);
+    book.rollbackAttributes();
+  }
+
+  @action
+  saveBook(book) {
+    if (book.isNotValid) {
+      return;
+    }
+
+    book.set('isEditing', false);
+    book.save();
+  }
+
+  @action
+  editAuthor(book) {
+    book.set('isAuthorEditing', true);
+  }
+
+  @action
+  cancelAuthorEdit(book) {
+    book.set('isAuthorEditing', false);
+    book.rollbackAttributes();
+  }
+
+  @action
+  saveAuthor(author, book) {
+    // Firebase adapter is buggy, we have to manually remove the previous relation
+    book.author.then((previousAuthor) => {
+      previousAuthor.books.then((previousAuthorBooks) => {
+        previousAuthorBooks.removeObject(book);
+        previousAuthor.save();
+      });
+    });
+
+    // Setup the new relation
+    book.set('author', author);
+    book.save().then(() => author.save());
+    book.set('isAuthorEditing', false);
+  }
+
+  @action
+  editLibrary(book) {
+    book.set('isLibraryEditing', true);
+  }
+
+  @action
+  cancelLibraryEdit(book) {
+    book.set('isLibraryEditing', false);
+    book.rollbackAttributes();
+  }
+
+  @action
+  saveLibrary(library, book) {
+    // Firebase adapter is buggy, we have to manually remove the previous relation.
+    // You don't need this callback mess when your adapter properly manages relations.
+    // If Firebase fix this bug, we can remove this part.
+    book.library.then((previousLibrary) => {
+      previousLibrary.books.then((previousLibraryBooks) => {
+        previousLibraryBooks.removeObject(book);
+        previousLibrary.save();
+      });
+    });
+
+    book.set('library', library);
+    book.save().then(() => library.save());
+    book.set('isLibraryEditing', false);
+  }
+}
