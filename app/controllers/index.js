6a656fa3d48617c24f6b0b46caceeb2c555dcecb
Zoltan <zdebre@gmail.com>
2020-03-09 18:35:59 +1300

    Update IndexController using tracked decorator

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,12 +1,13 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { match, not } from '@ember/object/computed';
+import { tracked } from '@glimmer/tracking';
 
 export default class HomeController extends Controller {
 
   headerMessage = 'Demo Home Page';
-  responseMessage = '';
-  emailAddress = '';
+  @tracked responseMessage = '';
+  @tracked emailAddress = '';
 
   @match('emailAddress', /^.+@.+\..+$/) isValid;
   @not('isValid') isDisabled;
@@ -15,9 +16,9 @@ export default class HomeController extends Controller {
   saveInvitation() {
     const newInvitation = this.store.createRecord('invitation', { email: this.emailAddress });
 
-    newInvitation.save().then((response) => {
-      this.set('responseMessage', `Thank you! We saved your email address with the following id: ${ response.id }`);
-      this.set('emailAddress', '');
+    newInvitation.save().then(response => {
+      this.responseMessage = `Thank you! We saved your email address with the following id: ${ response.id }`;
+      this.emailAddress = '';
     });
   }
 }
