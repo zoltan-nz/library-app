8bfb7f37b5e7e9be1d8146fefe3352eaf75b0422
Zoltan <zoltan-nz@users.noreply.github.com>
2020-03-20 11:04:22 +1300

    Migrate to ember-bootstrap #30 (#78)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,11 +4,16 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    // Add options here
     babel: {
       sourceMaps: 'inline',
     },
 
-    // Add options here
+    'ember-bootstrap': {
+      bootstrapVersion: 3,
+      importBootstrapCSS: false,
+      importBootstrapFont: true,
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
