cb3a321afe4ed1392628fcb376d7d77bbd79024a
Zoltan <zdebre@gmail.com>
2020-11-14 12:50:00 +1300

    Move font icons config in icons.js

--- config/environment.js
+++ config/environment.js
@@ -24,12 +24,6 @@ module.exports = function (environment) {
       'connect-src': "'self' wss://*.firebaseio.com https://*.googleapis.com",
     },
 
-    fontawesome: {
-      icons: {
-        'free-solid-svg-icons': 'all',
-      },
-    },
-
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
