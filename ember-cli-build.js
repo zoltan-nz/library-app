db40f35265aa48350b9d5ecab0f36fcae0eca432
Zoltan <zdebre@gmail.com>
2022-09-25 16:47:13 -0400

    Use new .args syntax and update service syntax

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,9 +5,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
-    babel: {
-      sourceMaps: 'inline',
-    },
 
     'ember-bootstrap': {
       bootstrapVersion: 5,
